d8c4d6dd6a941012055b7afc1ca3014f5ebcb475
rajlanjewar01 <rajl.perficient@gmail.com>
2026-04-30 18:06:43 +0530

    refactor: getting an action's creator type

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ const songsSlice = createSlice({
 	},
 
 	extraReducers(builder) {
-		builder.addCase("movie/reset", (state, action) => {
+		builder.addCase(moviesSlice.actions.reset, (state, action) => {
 			return [];
 		});
 	}
